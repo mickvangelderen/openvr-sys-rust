39fbe15ad63c9f6f7a89c70be95064b60215c60c
Mick van Gelderen <mickvangelderen@gmail.com>
2018-12-13 17:16:29 +0100

    Add hidden declarations are from openvr_capi.h
    Not sure why they're hidden.

--- wrapper.h
+++ wrapper.h
@@ -1,1 +1,12 @@
 #include "openvr/headers/openvr_capi.h"
+
+// Taken from openvr_capi.h
+
+// Global entry points
+S_API intptr_t VR_InitInternal( EVRInitError *peError, EVRApplicationType eType );
+S_API void VR_ShutdownInternal();
+S_API bool VR_IsHmdPresent();
+S_API intptr_t VR_GetGenericInterface( const char *pchInterfaceVersion, EVRInitError *peError );
+S_API bool VR_IsRuntimeInstalled();
+S_API const char * VR_GetVRInitErrorAsSymbol( EVRInitError error );
+S_API const char * VR_GetVRInitErrorAsEnglishDescription( EVRInitError error );
